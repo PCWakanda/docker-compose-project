d814d3a6606baa466eb09af5c87bb2696aee6b91
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-25 18:01:25 +0100

    Initial commit

---
+++ src/test/java/org/example/dockercomposeproject/DockerComposeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dockercomposeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
